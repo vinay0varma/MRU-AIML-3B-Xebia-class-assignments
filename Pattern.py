9a04d7fa7b3797fed9071b9569f721bd226a7400
vinay varma <159421550+vinay0varma@users.noreply.github.com>
2024-08-12 09:16:17 +0530

    Create Pattern.py

---
+++ Pattern.py
@@ -0,0 +1,5 @@
+#Print a Pattern
+#Write a function that prints a pattern of stars based on the number of rows provided. For example, if rows = 5, the output should be:
+a = int(input())
+for i in range(1,a+1):
+    print("*"*i)
